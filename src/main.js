210b791891d5350231e36c07d3f3c46b21f7e897
戴伟健 <daiweijian1013@hotmail.com>
2020-06-18 17:58:43 +0800

    减小js体积

--- src/main.js
+++ src/main.js
@@ -5,9 +5,6 @@ import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
